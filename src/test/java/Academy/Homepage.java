f29e8fb51d8f6d9be9b0412cb3dcb5e5ff5c991f
Dileep <dileepkumarpaidi@gmail.com>
2021-05-11 12:06:47 +0530

    failing multiple test cases

--- src/test/java/Academy/Homepage.java
+++ src/test/java/Academy/Homepage.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -37,6 +38,7 @@ public class Homepage extends base {
 		LoginPage lp=new LoginPage(driver);
 		lp.getemail().sendKeys(username);
 		lp.getpassword().sendKeys(password);
+		Assert.assertTrue(false);
 		lp.getsubmit().click();
 		System.out.println(text);
 		}
